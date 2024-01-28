9afe415634cdc7885924a46f04166fab7ac42e05
shazinaHijazy97 <zahrashazina97@gmail.com>
2024-01-28 17:23:36 +0530

    Login Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ToggleComponent } from "./toggle/toggle.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, ToggleComponent, DashboardComponent]
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, 
+      DashboardComponent, ToggleComponent, LoginComponent, RegistrationComponent]
 })
 export class AppComponent {
   title = 'my-app-test';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToggleComponent } from './toggle/toggle.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+    {path: ' ', component: AppComponent},
     {path: 'dashboard', component: DashboardComponent},
     {path: 'toggle', component: ToggleComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegistrationComponent},
+    {path: 'changepassword', component: ForgotPasswordComponent},
 ];
